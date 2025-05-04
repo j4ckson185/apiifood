b71caeb2f6ece85a01e72cd50793d9fb756856bc
j4ckson185 <jackson_division@hotmail.com>
2025-05-04 12:42:47 -0300

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,19 +1,29 @@
 // firebase-init.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
-import { getFirestore }  from "https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js";
+import {
+  initializeApp,
+  getApps,
+  getApp
+} from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
+
+import {
+  getFirestore
+} from "https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js";
+
+import {
+  getAuth
+} from "https://www.gstatic.com/firebasejs/11.6.1/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBuRHTtuKgpQE_qAdMIbT9_9qbjh4cbLI8",
   authDomain: "apiifood-e0d35.firebaseapp.com",
   projectId: "apiifood-e0d35",
-  storageBucket: "apiifood-e0d35.firebasestorage.app",
+  storageBucket: "apiifood-e0d35.appspot.com",
   messagingSenderId: "905864103175",
   appId: "1:905864103175:web:a198383d3a66a7d2cd31a2"
 };
 
-// inicializa o App
-const app = initializeApp(firebaseConfig);
-// inicializa o Firestore
-const db = getFirestore(app);
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+const db  = getFirestore(app);
+const auth = getAuth(app);
 
-export { db };
+export { db, auth };
