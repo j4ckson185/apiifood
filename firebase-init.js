bec4ad8d4bc5f22de8346c9ec7000e99e92d5df2
j4ckson185 <jackson_division@hotmail.com>
2025-05-04 10:17:48 -0300

    Create firebase-init.js

---
+++ firebase-init.js
@@ -0,0 +1,23 @@
+// firebase-init.js
+// Inicializa o Firebase App e exporta a instância do Firestore (db)
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
+import { getFirestore }  from "https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBuRHTtuKgpQE_qAdMIbT9_9qbjh4cbLI8",
+  authDomain: "apiifood-e0d35.firebaseapp.com",
+  projectId: "apiifood-e0d35",
+  storageBucket: "apiifood-e0d35.firebasestorage.app",
+  messagingSenderId: "905864103175",
+  appId: "1:905864103175:web:a198383d3a66a7d2cd31a2"
+};
+
+// Inicializa o Firebase App
+const app = initializeApp(firebaseConfig);
+
+// Inicializa o Firestore
+const db = getFirestore(app);
+
+// Exporta para uso em outros módulos
+export { db };
